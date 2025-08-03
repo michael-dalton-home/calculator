cf4ab9bb4d5f10e2733f53fcadd7534c31261592
Michael Dalton <215679523+michael-dalton-home@users.noreply.github.com>
2025-08-03 11:34:51 +0100

    Key listener basics
    Added listener to all keys except '.' and wired up
    to the display.  No logic to perform the calcs yet though

--- js/javascript.js
+++ js/javascript.js
@@ -1,3 +1,12 @@
+const keyArea = document.querySelector("#key-area");
+const display = document.querySelector("#digi-display");
+const modifier= document.querySelector("#digi-modifier");
+
+const DEF_DISP = '0';       // Default display sequence
+const DISP_LEN = 8;         // Max number of characters in display
+
+let accumulator = 0;        // Total so far
+
 
 function buildKeysMatrix() {
     const keyMatrix = 
@@ -7,7 +16,6 @@ function buildKeysMatrix() {
             [1,2,3,'-'],
             [0,'.','=','+']
         ];
-    const keyArea = document.querySelector("#key-area");
     let row, key;
 
     for (r=0;r<keyMatrix.length;r++) {
@@ -25,10 +33,57 @@ function buildKeysMatrix() {
    }
 }
 
+function handleKeyPress(ev) {
+    console.log('Key '+ev.target.textContent);
+
+    switch (ev.target.textContent) {
+        case '0':
+        case '1':
+        case '2':
+        case '3':
+        case '4':
+        case '5':
+        case '6':
+        case '7':
+        case '8':
+        case '9':
+            if (display.textContent === DEF_DISP)
+                display.textContent = ev.target.textContent;
+            else if (display.textContent.length < 8)
+                display.textContent += ev.target.textContent;
+            break;
+        case '+':
+        case '-':
+        case "\u00f7":      // Divide
+        case 'x':
+            if (modifier.textContent === '') {
+                //calculate(ev.target.textContent);
+                display.textContent = DEF_DISP;
+            }
+            modifier.textContent = ev.target.textContent;
+            break;
+        case 'AC':
+            modifier.textContent = '';
+            display.textContent = DEF_DISP;
+            break;
+        case '\u2190':      // Backspace
+            display.textContent = display.textContent.slice(0, -1);
+            if (display.textContent === '')
+                display.textContent = DEF_DISP;
+            break;
+        case '=':
+            if (modifier.textContent) {
+                //calculate(ev.target.textContent);
+            }
+            break;
+    }
+}
 
 /** MAIN 
  * 
  */
 {
     buildKeysMatrix();
+    display.textContent = DEF_DISP;
+    keyArea.addEventListener('click', handleKeyPress);
 }
